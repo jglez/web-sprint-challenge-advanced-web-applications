d7b1edf5dafe0e4ccf9d99231c438c228eb16728
jglez <jglez.cs@gmail.com>
2021-02-26 19:59:31 -0600

    Created PrivateRoute to protect BubblePage. Redirects to login page if token is falsy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import BubblePage from './components/BubblePage'
 import Login from "./components/Login";
+import PrivateRoute from './components/PrivateRoute'
 import "./styles.scss";
 
 function App() {
   return (
-    <Router>
+    <>
       <div className="App">
         <Route exact path="/" component={Login} />
-      </div>
 
-      <Route exact path='/bubbles' component={BubblePage} />
-    </Router>
+        <PrivateRoute exact path='/bubbles' component={BubblePage} />
+      </div>
+    </>
   );
 }
 
 export default App;
 
 //Task List:
-//1. Render BubblePage as a PrivateRoute
+//1. Render BubblePage as a PrivateRoute ✅
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,21 @@
-
-
-
-
-
-
-
-
-
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom'
 //Task List:
-//1. Build a PrivateRoute component that redirects if user is not logged in
+//1. Build a PrivateRoute component that redirects if user is not logged in ✅
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (localStorage.getItem('token')) {
+          return <Component />;
+        } else {
+          // route to login
+          return <Redirect to='/' />
+        }
+      }} />
+  )
+}
+
+export default PrivateRoute
